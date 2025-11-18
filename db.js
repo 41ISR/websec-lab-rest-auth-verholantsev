3438a67575900025d810b70982a8108e5d7305fe
UltraSlav <pashaverholantsev@gmail.com>
2025-11-18 04:25:54 +0000

    1234

---
+++ db.js
@@ -0,0 +1,50 @@
+const Database = require("better-sqlite3")
+
+const db = new Database("database.db")
+
+db.pragma('foreign_keys = ON');
+
+db.exec(
+    `
+    CREATE TABLE IF NOT EXISTS User (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `
+)
+
+db.exec(
+    `
+    CREATE TABLE IF NOT EXISTS Book (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        year INTEGER NOT NULL,
+        genre TEXT NOT NULL,
+        description TEXT NOT NULL,
+        createdBy INTEGER NOT NULL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (createdBy) REFERENCES User(id) ON DELETE CASCADE
+    )
+    `
+)
+
+db.exec(
+    `
+    CREATE TABLE IF NOT EXISTS Review (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        bookId INTEGER NOT NULL,
+        userId INTEGER NOT NULL,
+        rating INTEGER NOT NULL,
+        comment TEXT NOT NULL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (bookId) REFERENCES Book(id) ON DELETE CASCADE,
+        FOREIGN KEY (userId) REFERENCES User(id) ON DELETE CASCADE
+    )
+    `
+)
+
+module.exports = db
